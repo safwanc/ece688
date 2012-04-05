d8fc4949ba1999792b2c5115aa95cd0b4d4349b3
Safwan Choudhury <im@safwanc.com>
2012-04-05 09:56:57 -0400

    Something slightly off about stable region 2.. Used a hack

--- stableregions.m
+++ stableregions.m
@@ -17,7 +17,7 @@ hspr = plot([-B/2*thval, NaN, B/2*thval], [dthval, NaN, dthval], ...
 %% Stable Region 1 [Inside Energy Well]
 
 thval = -B/2 : DEL : B/2;                           % -B/2 < Theta0 < B/2
-sr1 = @(th) sqrt( (2*mgL*(1-cosd(abs(th)-B/2))) / (Icom + mL2) ); % Eq 27
+sr1 = @(th) sqrt( (2*mgL*(1-cosd(abs(th)-B/2))) / (Icom + mL2) ); % [Eq 27]
 
 sr1pos = sr1(thval);    % Positive root
 sr1neg = -1 * sr1pos;   % Negative root
@@ -31,28 +31,20 @@ thvala  = -(B/2)+DEL : DEL : 0-DEL;               % -B/2 < Theta0 < 0
 %dthvala = 0 : DEL : dthrange(HIGH);     % dTheta0 >= 0
 
 thvalb  = 0+DEL : DEL : (B/2)-DEL;                % 0 < Theta0 < B/2
-%dthvalb = dthrange(LOW) : DEL : 0;      % dTheta0 <= 0
+%dthvalb = dthrange(LOW) : DEL : 0;      % 
 
-sr2 = @(arg) (((2*mgL)/(Icom + mL2)) * (- cosd(arg) - cosd(B/2) + ...
+sr2 = @(arg) sqrt(((-2*mgL)/(Icom + mL2)) * (- cosd(arg) - cosd(B/2) + ...
     ( ( (1-cosd(B/2))*((Icom + mL2)^2) ) / ( (mL2*cosd(B)+Icom)^2) ) )); 
 
-a = (2*mgL) / (Icom + mL2); 
-b = 1 - cosd(B/2); 
-c = (Icom + mL2)^2; 
-d = ((mL2*cosd(B)) + Icom)^2; 
-e = cosd(B/2); 
+dthvala =   sr2(thvala + B/2);  % dTheta0 >= 0 [Eq 38]
+dthvalb =  -sr2(thvalb - B/2);  % dTheta0 <= 0 [Eq 39]
 
-sr2t = @(arg) a * ( (b*c/d) - cosd(arg) - e); 
-
-dthvala =   sr2(thvala + B/2);  % Eq 38
-dthvalb =  -sr2(thvalb - B/2);  % Eq 39
-
-%hsr2 = plot([thvala, NaN, thvalb], [dthvala, NaN, dthvalb], ...
-%    'm--', 'LineWidth', 2); 
+hsr2 = plot([thvala, NaN, thvalb], [dthvala, NaN, dthvalb], ...
+   'm--', 'LineWidth', 2); 
 
 %% Stable Region 3 [Outside Energy Well] 
 
 
 
 %% Legends
-legend(hsr1, 'Stable Region 1')
+legend([hsr1 hsr2], 'Stable Region 1', 'Stable Region 2')
