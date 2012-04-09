a8d833e7b87f122d70e02a7c7c10ff9cff517b9e
Safwan Choudhury <im@safwanc.com>
2012-04-09 00:52:31 -0400

    Adding script to run all simulations

---
+++ RunSimulations.m
@@ -0,0 +1,38 @@
+% RUNSIMULATIONS Executes simulations to produce all plots/results
+%
+% Course:       ECE 688
+%               Nonlinear Systems
+%
+% See also:     BIPED2DOF
+
+% Author:       Safwan Choudhury, MASc
+%               University of Waterloo
+%
+% Date:         April 10, 2012
+%
+% Email:        schoudhu@uwaterloo.ca
+% Website:      http://ece.uwaterloo.ca/~schoudhu
+
+%% Workspace
+close all; clear all; clc
+
+HTimePlot   = 1;    % Figure handle for 'Time Evolution Plots'
+HPhasePlot  = 2;    % Figure handle for 'Phase Portrait Plots'
+
+fprintf(1,'\nECE 688: Course Project Simulation\n');
+
+%% Init + Simulate 
+Initialize2DOF; Simulate(Biped, [-29; 0]); 
+
+%% Produce Plots
+
+% Display time evolution of uniform state variable
+TimeEvolution(Biped, HTimePlot); 
+
+% Display phase portrait with stable regions outlined. 
+PhasePortrait(Biped, HPhasePlot); 
+
+% @TODO: Display state trajectories overlaid 
+
+%% Clean
+clear H*; 
